4bee69e33e38e0824cb1a9d23089827a2b37817c
Frederick Roegiers <frederick@rogerthat.be>
2020-03-16 12:01:55 +0100

    [WIP]

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+// 1. functie declareren met 4 parameters 
+predictFuture(jobTitle, allLocation, partner, amountOfKids) {
+
+}
+
+// 2. in de functie moet een volzin gemaakt worden, die "geprint" wordt naar de DOM
+// via template literals: 
+    const someVariable = 'voorbeeld'; 
+    const someString = `dit is een ${someVariable}`;
+
+
+// 3: roep die functie op
+predictFuture('a', 'Mariakerke', .......);
